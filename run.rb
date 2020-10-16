c9644c38634897a1928b60cc081dc6b76159ad63
Captain-Vorobey <vorobyov_egor@bk.ru>
2020-10-16 14:12:44 +0300

    Replacesd nokogiri on capybara

--- run.rb
+++ run.rb
@@ -1,14 +1,29 @@
 #!/usr/bin/env ruby
 
+require 'capybara'
+
 require_relative 'lib/adding_parsed_array'
 require_relative 'lib/article_parser'
 require_relative 'data/data.rb'
 
+# webdriver configuration for Gecko
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.javascript_driver = :chrome
+Capybara.configure do |config|
+  config.default_max_wait_time = 10
+  config.default_driver = :selenium
+end
+
 puts 'Wait a bit please...'
 
-html = Curl.get(URL)
+browser = Capybara.current_session
+driver = browser.driver.browser
+browser.visit URL
 
-array = AddingParsedArray.new([], html).push_to_articles
+array = AddingParsedArray.new([], browser).push_to_articles
 
 def array.arr_to_file(arr)
   ParsePage.new.parse_page(FILE_NAME, arr)
