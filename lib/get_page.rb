d3ce0a9f4d3b118b5777bb305692d3fcde56767a
Captain-Vorobey <vorobyov_egor@bk.ru>
2020-10-16 17:58:27 +0300

    Test

--- lib/get_page.rb
+++ lib/get_page.rb
@@ -2,11 +2,24 @@ require 'nokogiri'
 require 'open-uri'
 require 'xpath'
 require 'curb'
+require 'capybara'
+require 'xpath'
 
 require_relative '../data/data.rb'
 
 module NokogiriReader
   def self.get_page
-    doc = Nokogiri::HTML(open(URL))
-  end
+    Capybara.register_driver :selenium do |app|  
+      Capybara::Selenium::Driver.new(app, browser: :chrome)
+    end
+    Capybara.javascript_driver = :chrome
+    Capybara.configure do |config|  
+      config.default_max_wait_time = 10 # seconds
+      config.default_driver = :selenium
+    end
+    # Visit
+    browser = Capybara.current_session
+    driver = browser.driver.browser
+    doc = Nokogiri::HTML(driver.page_source);
+   end
 end
--- run.rb
+++ run.rb
@@ -1,29 +1,14 @@
 #!/usr/bin/env ruby
 
-require 'capybara'
-
 require_relative 'lib/adding_parsed_array'
 require_relative 'lib/article_parser'
 require_relative 'data/data.rb'
 
-# webdriver configuration for Gecko
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.javascript_driver = :chrome
-Capybara.configure do |config|
-  config.default_max_wait_time = 10
-  config.default_driver = :selenium
-end
-
 puts 'Wait a bit please...'
 
-browser = Capybara.current_session
-driver = browser.driver.browser
-browser.visit URL
+html = Curl.get(URL)
 
-array = AddingParsedArray.new([], browser).push_to_articles
+array = AddingParsedArray.new([], html).push_to_articles
 
 def array.arr_to_file(arr)
   ParsePage.new.parse_page(FILE_NAME, arr)
